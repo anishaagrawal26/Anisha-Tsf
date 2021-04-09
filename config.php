ba0f6f45be445b74c09dc785dc24b810e5a521ef
Anisha Agrawal <81238442+anishaagrawal26@users.noreply.github.com>
2021-04-09 11:12:06 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    
+
+	$conn = mysqli_connect('localhost','id16557745_anishaagrawal970','Abchurakardikhao12-','id16557745_anisha');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+	
+
+?>
