f837e1c7d8a8285c936a2989839bca90aa73e5a1
Anisha Agrawal <81238442+anishaagrawal26@users.noreply.github.com>
2021-04-09 11:14:02 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Basic Banking System</title>
+  <style>
+	.footer {
+   
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: black;
+   text-align: center;
+   background-color :lightgreen;
+}
+	
+	</style>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div >
+            <div>
+                <div class="heading text-center my-4">
+                  <h3 style=" color:Black ; background-color: lightblue">Welcome to</h3>
+                  <h1 style=" color:Black ; background-color: lightblue">SPARKS  COOPERATION BANK</h1>
+                </div>
+              </div>
+            </div>
+			<div class="container-fluid" >
+			<div class="container-fluid-sm" >
+               <img src="image/bank1.jpg" style=" width:100%;height:10%">
+     </div></div>
+      </div>
+      <div class="footer">
+        <p> Made by <b>ANISHA AGRAWAL</b> <br> The Sparks Foundation</p>
+		<p><a href="http://www.github.com/anishaagrawal26"><i class="fa fa-github" style="font-size:36px"></i></a>
+		<a href="http://www.linkedin.com/in/anisha-agrawal-b039581a0"><i class="fa fa-linkedin-square" style="font-size:36px;color:blue"></i></a>
+		</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+ </body>
+</html>
